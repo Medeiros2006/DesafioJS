37a09184e7b0cf2c914df8a177149c3999baeb7a
Medeiros2006 <vcostavitoriac@gmail.com>
2023-05-19 08:48:54 -0300

    first commit

---
+++ codigos.js
@@ -0,0 +1,23 @@
+
+
+
+function ativarNome(){
+    let nome = 'Medeiros';
+    console.log(nome)
+     
+    document.getElementById('nome').innerHTML = nome
+
+}
+
+
+function somarNumeros(){
+    let a = 6;
+    let b = 8;
+    let c = a*b;
+
+
+    console.log(c)
+    
+    document.getElementById('aparecer').innerHTML = c
+}
+
